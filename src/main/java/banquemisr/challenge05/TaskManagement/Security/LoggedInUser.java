f3a6d9263be4e7838ab7244c3943de4caf000bc2
Islam Magdy <43723746+Islam99@users.noreply.github.com>
2024-12-04 20:03:29 +0200

    Delete src directory

--- src/main/java/banquemisr/challenge05/TaskManagement/Security/LoggedInUser.java
+++
@@ -1,9 +0,0 @@
-package banquemisr.challenge05.TaskManagement.Security;
-
-import java.lang.annotation.*;
-
-@Target(ElementType.PARAMETER)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
-public @interface LoggedInUser{
-        }
